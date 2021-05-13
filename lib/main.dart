49bc8d04e338f714ec71380c92d9654cc5264c70
fangju <fangju@wepie.com>
2021-05-13 10:31:36 +0800

    debug

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,9 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
+
+//导入cupertino widget库
+import 'package:flutter/cupertino.dart';
 
 void main() => runApp(new MyApp());
 
@@ -9,7 +14,7 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: new ThemeData(primarySwatch: Colors.blue),
       routes: {
-        "new_page": (context) => NewRoute(),
+        "new_page": (context) => TapboxA(),
         "/": (context) => new MyHomePage(title: 'Flutter Demo Home Page')
       },
     );
@@ -69,13 +74,159 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 }
 
-class EchoRouter extends StatelessWidget{
+class ParentWidget extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _ParentWidgetState();
+}
+
+class _ParentWidgetState extends State<ParentWidget> {
+  bool _active = false;
+
+  void _handleTapboxChanged(bool newValue) {
+    setState(() {
+      _active = newValue;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: TapboxB(
+        active: _active,
+        onChanged: _handleTapboxChanged,
+      ),
+    );
+  }
+}
+
+class TapboxB extends StatelessWidget {
+  TapboxB({Key key, this.active: false, @required this.onChanged})
+      : super(key: key);
+
+  final bool active;
+  final ValueChanged<bool> onChanged;
+
+  void _handleTap() {
+    onChanged(!active);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: _handleTap,
+      child: Container(
+        child: Center(
+          child: Text(
+            active ? 'Active' : "Inactive",
+            style: TextStyle(fontSize: 32.0, color: Colors.white),
+          ),
+        ),
+        width: 200.0,
+        height: 200.0,
+        decoration: BoxDecoration(
+            color: active ? Colors.lightGreen[700] : Colors.grey[600]),
+      ),
+    );
+  }
+}
+
+class TapboxA extends StatefulWidget {
+  @override
+  _TapboxAState createState() => _TapboxAState();
+}
+
+class _TapboxAState extends State<TapboxA> {
+  bool _active = false;
+
+  void _handleTap() {
+    setState(() {
+      _active = !_active;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: _handleTap,
+      child: Container(
+        child: Center(
+          child: Text(
+            _active ? 'Active' : "Inactive",
+            style: TextStyle(fontSize: 32.0, color: Colors.white),
+          ),
+        ),
+        width: 200.0,
+        height: 200.0,
+        decoration: BoxDecoration(
+            color: _active ? Colors.lightGreen[700] : Colors.grey[600]),
+      ),
+    );
+  }
+}
+
+class CounterWidget extends StatefulWidget {
+  const CounterWidget({Key key, this.initValue = 0});
+
+  final int initValue;
+
+  @override
+  State<StatefulWidget> createState() => _CounterWidgetState();
+}
+
+class _CounterWidgetState extends State<CounterWidget> {
+  int _counter;
+
+  @override
+  void initState() {
+    super.initState();
+    // 初始化状态
+    _counter = widget.initValue;
+    print("initState");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    print("build");
+    return Scaffold(
+      body: Center(
+        child: FlatButton(
+          child: Text('$_counter'),
+          onPressed: () => setState(() => ++_counter),
+        ),
+      ),
+    );
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    print("didChangeDependencies");
+  }
+
+  @override
+  void deactivate() {
+    super.deactivate();
+    print("deactive");
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    print("dispose");
+  }
+
+  @override
+  void reassemble() {
+    super.reassemble();
+    print("reassemble");
+  }
+}
+
+class EchoRouter extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var args = ModalRoute.of(context).settings.arguments;
-
   }
-
 }
 
 class NewRoute extends StatelessWidget {
@@ -135,6 +286,23 @@ class RouterTextRouter extends StatelessWidget {
           print("路由返回值: $result");
         },
         child: Text("打开提示页"),
+      ),
+    );
+  }
+}
+
+class CupertinoTestRoute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(
+      navigationBar: CupertinoNavigationBar(
+        middle: Text("Cupertino Demo"),
+      ),
+      child: Center(
+        child: CupertinoButton(
+            color: CupertinoColors.activeBlue,
+            child: Text("Press"),
+            onPressed: () {}),
       ),
     );
   }
